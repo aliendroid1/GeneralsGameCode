29ace6dba201e870ffd497653de73cb6001bcb60
aliendroid1 <9aliquraishi9@gmail.com>
2025-05-15 18:47:20 -0500

    fixing empty vs clear mix up

--- Generals/Code/GameEngine/Source/GameNetwork/GameSpy/Thread/PeerThread.cpp
+++ Generals/Code/GameEngine/Source/GameNetwork/GameSpy/Thread/PeerThread.cpp
@@ -2222,7 +2222,7 @@ static void listGroupRoomsCallback(PEER peer, PEERBool success,
 		}
 		else
 		{
-			resp.groupRoomName.empty();
+			resp.groupRoomName.clear();
 		}
 		TheGameSpyPeerMessageQueue->addResponse(resp);
 #ifdef SERVER_DEBUGGING
